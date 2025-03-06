08a97cb434bd3e24816c804c97e3af6138a69039
ajaySingh2615 <ajaysingh261526@gmail.com>
2025-03-06 20:11:56 +0530

    add printMaxSubArraySum method to OptimizeCode class for efficient maximum subarray sum calculation

--- src/com/arrayspartone/OptimizeCode.java
+++ src/com/arrayspartone/OptimizeCode.java
@@ -1,19 +1,19 @@
 package com.arrayspartone;
 
 public class OptimizeCode {
-    public static void printBruteCodeForce(int[] arr){
+    public static void printBruteCodeForce(int[] arr) {
         int sum = 0;
         int maximum_sum = Integer.MIN_VALUE;
 
         for (int i = 0; i < arr.length; i++) {
             sum = 0;
             for (int j = i; j < arr.length; j++) {
-              for (int k = i; k <= j; k++) {
+                for (int k = i; k <= j; k++) {
                     System.out.print(arr[k] + " ");
                     sum += arr[k];
-              }
-              maximum_sum = Math.max(maximum_sum, sum);
-              System.out.println();
+                }
+                maximum_sum = Math.max(maximum_sum, sum);
+                System.out.println();
             }
             System.out.println();
         }
@@ -21,9 +21,35 @@ public class OptimizeCode {
         System.out.println("Maximum :" + maximum_sum);
     }
 
-    public static void main(String[] args) {
-        int[] arr = {1, -2, 6, -1, 3};
+    public static void printMaxSubArraySum(int[] arr) {
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+        int[] prefix = new int[arr.length];
 
-        printBruteCodeForce(arr);
+        // calculate prefix arr
+        prefix[0] = arr[0];
+        for (int i = 1; i < prefix.length; i++) {
+            prefix[i] = prefix[i - 1] + arr[i];
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+
+                currSum = i == 0 ? prefix[j] : prefix[j] - prefix[i - 1];
+
+                if (maxSum < currSum) {
+                    maxSum = currSum;
+                }
+            }
+
+        }
+        System.out.println("max sum = " + maxSum);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, -2, 6, -1, 3 };
+
+        // printBruteCodeForce(arr);
+        printMaxSubArraySum(arr);
     }
 }
